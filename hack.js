57a7279209b6c5ae2611af6901b8b1dcd1d7eada
zhengzonggang <zhengzonggang@qudian.com>
2024-02-21 15:34:04 +0800

    script update

--- hack.js
+++ hack.js
@@ -11,7 +11,7 @@ content = fs.readFileSync(path, { encoding: 'utf-8'})
 m = content.match(/const url = new URL.*\n.*encodeURL\)\(url\);/gm)
 if (m != null) {    
     console.log(m)
-    content = content.replace(/const url = new URL.*\n.*encodeURL\)\(url\);/gm, "const link = url_for.call(ctx, post.path + (hash ? `#${hash}` : ''));")
+    content = content.replace(/const url = new URL.*\n.*encodeURL\)\(url\);/gm, "const link = hexo_util_1.url_for.call(ctx, post.path + (hash ? `#${hash}` : ''));")
     // content = content.replace(/const devices = \(0, _parseIOSDevicesList.default\)\(_child_process.+\n(.+\n)*/gm, `const devices = (0, _parseIOSDevicesList.default)(_execa().default.sync('xcrun', ['xctrace', 'list', 'devices']));`)    
     fs.writeFileSync(path, content)
 }
